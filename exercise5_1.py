143608f0ebf1914dd0f101b920321e3cb1f11221
christinebntz <82741329+christinebntz@users.noreply.github.com>
2021-04-18 10:36:22 +0800

    Update exercise5_1.py

--- exercise5_1.py
+++ exercise5_1.py
@@ -19,44 +19,25 @@ by Charles R. Severance
 """
 
 
-def check_for_float(input1, exit = True):
-    """
-    Checks if the type of "input1" is a float and returns the value if so.
-    Input:    input1 -- variable to check
-    Output: val -- value of float
-    """
+print('Please input random numbers. Then, input "done" if you wish to end the loop.')
+
+sum = 0 
+ctr = 0
+avg = 0
+
+while True:
     try:
-        val = float(input1)                   # Only allows input floats
-        return val
-    except (ValueError, TypeError):
-        print('Error, please enter numeric input')
-        if exit:
-            quit()
-        return False
+        num = input('Input a number: ')
+        if num == 'done':
+            break
+        else:
+            num1 = float(num) 
+            ctr = ctr + 1 
+            sum = sum + num1
+            avg = sum / ctr
+    except:
+        print('Invalid input.')
 
-
-# Check module name since check_for_float is being imported in the next
-# exercise. See also, https://www.youtube.com/watch?v=sugvnHA7ElY (video)
-# or https://www.tutorialspoint.com/python/python_modules.htm (text)
-if __name__ == "__main__":
-    count = 0                                 # Initializes values
-    total = 0.0
-    average = 0.0
-
-    while True:                               # Stays in loop until break
-        input_number = input('Enter a number: ')
-        if input_number == 'done':
-            break                             # Exits the while loop
-
-        number = check_for_float(input_number, False)
-        if not number:
-            continue
-
-        count += 1                            # Counter
-        total = total + number                # Running total
-
-    # Ensures count is not 0 which would cause division error
-    if count:
-        average = total / count               # Computes the average
-
-    print(total, count, average)
+print('The count is ', ctr)
+print('The sum is ', sum)
+print('The average is ', avg)
